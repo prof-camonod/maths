ece6ae6a2ce47c4936e8968703343bc4297e1be4
prof.camonod <>
2026-04-23 21:19:54 +0200

    menu

--- flashcards.js
+++ flashcards.js
@@ -2,16 +2,24 @@ let flashcards = [];
 let index = 0;
 let showAnswer = false;
 
-// Charger le JSON
-fetch("cards.json")
-  .then(response => response.json())
-  .then(data => {
-    flashcards = data;
-    displayCard();
-  });
+function loadDeck() {
+  const selectedFile = document.getElementById("deckSelector").value;
+
+  fetch(selectedFile)
+    .then(response => response.json())
+    .then(data => {
+      flashcards = data;
+      index = 0;
+      showAnswer = false;
+      displayCard();
+    });
+}
 
 function displayCard() {
-  if (flashcards.length === 0) return;
+  if (flashcards.length === 0) {
+    document.getElementById("content").textContent = "Aucune carte";
+    return;
+  }
 
   const card = flashcards[index];
   document.getElementById("content").textContent =
@@ -19,17 +27,20 @@ function displayCard() {
 }
 
 function flipCard() {
+  if (flashcards.length === 0) return;
   showAnswer = !showAnswer;
   displayCard();
 }
 
 function nextCard() {
+  if (flashcards.length === 0) return;
   index = (index + 1) % flashcards.length;
   showAnswer = false;
   displayCard();
 }
 
 function prevCard() {
+  if (flashcards.length === 0) return;
   index = (index - 1 + flashcards.length) % flashcards.length;
   showAnswer = false;
   displayCard();
