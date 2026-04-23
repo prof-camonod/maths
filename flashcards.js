3775832a35c9dce03211cd90b885dce638ebff15
prof.camonod <>
2026-04-23 20:12:06 +0200

    debut

---
+++ flashcards.js
@@ -0,0 +1,36 @@
+let flashcards = [];
+let index = 0;
+let showAnswer = false;
+
+// Charger le JSON
+fetch("flashcards.json")
+  .then(response => response.json())
+  .then(data => {
+    flashcards = data;
+    displayCard();
+  });
+
+function displayCard() {
+  if (flashcards.length === 0) return;
+
+  const card = flashcards[index];
+  document.getElementById("content").textContent =
+    showAnswer ? card.answer : card.question;
+}
+
+function flipCard() {
+  showAnswer = !showAnswer;
+  displayCard();
+}
+
+function nextCard() {
+  index = (index + 1) % flashcards.length;
+  showAnswer = false;
+  displayCard();
+}
+
+function prevCard() {
+  index = (index - 1 + flashcards.length) % flashcards.length;
+  showAnswer = false;
+  displayCard();
+}
